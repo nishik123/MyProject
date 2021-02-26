1032e8370855c1d17d1e22883f6e5dfcd755f700
Nishi <soumy@LAPTOP-QE02DHC6>
2021-02-26 12:41:00 +0530

    M4() committed

--- MyApp/src/com/nt/beans/Utility.java
+++ MyApp/src/com/nt/beans/Utility.java
@@ -3,9 +3,12 @@ package com.nt.beans;
 import java.util.Date;
 
 public class Utility {
-	
+
 	public Date getSysDate() {
 		return new Date();
 	}
 
+	public void m4() {
+		System.out.println("asdfghjk");
+	}
 }
